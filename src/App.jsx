948af373d01a9e26c0d601bead8a2c49c98b7219
keshavnagar <keshavnagar887@gmail.com>
2025-10-29 21:46:51 +0530

    use combination of import and export

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Practice from "./interview/Practice";
-import NetflixCard ,{ Header,  Footer } from "./components/NetflixCard";
+import NetflixCard, { Header, Footer } from "./components/NetflixCard";
 // here i add netflix card outside because it is combo of both default and named import if i get error if i put the name exports outside the {} it give error same way if we add default export into {} so it also produce error, that's why we use in this way and it looks combination of both default and named components
 const App = () => {
   return (
     <>
+      <Header />
       <NetflixCard />
       <NetflixCard />
       <Footer />
