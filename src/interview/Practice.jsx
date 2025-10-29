9656bf273e63e078b8d2b46e584b7918de29bd8e
keshavnagar <keshavnagar887@gmail.com>
2025-10-29 19:22:12 +0530

    here the second slution to solve the not found student probemelm

--- src/interview/Practice.jsx
+++ src/interview/Practice.jsx
@@ -3,7 +3,8 @@ const Practice = () => {
   return (
     <>
       <h1>Practice Interview Quetions: </h1>
-      <p>{!students.length && "no students found"}</p>
+      {/* <p>{!students.length && "no students found"}</p> */}
+      <p>{students.length === 0 && "no students found"}</p>
       <p>number of students: {students.length}</p>
     </>
   );
