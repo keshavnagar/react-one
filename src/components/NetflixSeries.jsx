229a7034fc5b6785368656ba9e2b4c92c629bee0
keshavnagar <keshavnagar887@gmail.com>
2025-10-31 00:57:05 +0530

    create new component SeriesCard.jsx and put the data that is in the map method in NetflixSeries component and pass the series data through props and also pass the key as props

--- src/components/NetflixSeries.jsx
+++ src/components/NetflixSeries.jsx
@@ -1,26 +1,10 @@
 import seriesData from "../api/seriesData.json";
+import SeriesCard from "./SeriesCard";
 const NetflixSeries = () => {
   return (
     <ul>
       {seriesData.map((series) => (
-        <li key={series.id}>
-          <div>
-            <img
-              width={400}
-              height={200}
-              src={series.img_url}
-              alt={series.name}
-            />
-          </div>
-          <h1>Name: {series.name}</h1>
-          <h2>Rating: {series.rating}</h2>
-          <p>Desc: {series.description}</p>
-          <p>Genre: {series.genre}</p>
-          <p>Cast: {series.cast}</p>
-          <a href={series.watch_url} target="_blank">
-            <button>watch now</button>
-          </a>
-        </li>
+        <SeriesCard series={series} key={series.id} />
       ))}
     </ul>
   );
---
+++ src/components/SeriesCard.jsx
@@ -0,0 +1,18 @@
+const SeriesCard = ({series}) => {
+  return (
+    <li style={{listStyle: "none"}}>
+      <div>
+        <img width={400} height={200} src={series.img_url} alt={series.name} />
+      </div>
+      <h1>Name: {series.name}</h1>
+      <h2>Rating: {series.rating}</h2>
+      <p>Desc: {series.description}</p>
+      <p>Genre: {series.genre}</p>
+      <p>Cast: {series.cast}</p>
+      <a href={series.watch_url} target="_blank">
+        <button>watch now</button>
+      </a>
+    </li>
+  );
+};
+export default SeriesCard;
