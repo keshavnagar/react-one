4df8bf7b038bd2b13b72755fc1e985f6994453f5
keshavnagar <keshavnagar887@gmail.com>
2025-11-01 20:06:58 +0530

    we also add the inline css property also here

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -2,23 +2,25 @@ import "./SeriesCard.css";
 const SeriesCard = ({ series }) => {
   const { name, rating, description, genre, cast, watch_url, img_url } = series;
   const buttonStyle = {
-    backgroundColor: "var(--main-color)",
+    backgroundColor: rating>=8.5 ? "#31694E" : "#F0E491",
     padding: "10px",
     border: "0",
-    color: "var(--white-color)",
+    color: rating>=8.5?"#F0E491":  "#31694E" ,
     fontWeight: "bold",
     textTransform: "uppercase",
     letterSpacing: "2px",
     fontSize: "1.5rem",
   };
+  const condiStyle = rating >= 8.5 ? "green" : "yellow";
+  //it catch the value green or yellow and i pass through this condiStyle in our button and rating
   return (
     <li className="grid-item">
       <img width={400} height={200} src={img_url} alt={name} />
       <div className="grid-item-details">
-        <h1  >
+        <h1>
           <strong>Name:</strong> {name}
         </h1>
-        <p className={`${rating >= 8.5 ? "green" : "yellow"}`}>
+        <p className={condiStyle}>
           <strong>Rating:</strong> {rating}
         </p>
         <p>
@@ -31,7 +33,7 @@ const SeriesCard = ({ series }) => {
           <strong>Cast:</strong> {cast}
         </p>
         <a href={watch_url} target="_blank">
-          <button className={`${rating >= 8.5 ? "green" : "yellow"}`}>watch now</button>
+          <button style={buttonStyle} className={`${condiStyle}`}>watch now</button>
         </a>
       </div>
     </li>
---
+++ src/notes/lecture20/1condiStyling.md
@@ -0,0 +1,15 @@
+so in town awareness project i used conditional styling in my way: 
+in which I give the class name two different and manage them with condtinon like if the conditno is true than the fierst class is aapear otherwise second class
+
+# css conditional stylings in react js
+
+we can use ternary operators to add conditional stylings to our css
+
+we also put logical operators also here 
+
+but one thing in our code again that is also we discuss in previous chapater that is we apply this inline even it is classname not inline but also it renders again and again and not look good here
+
+so we declare that thing in above in our component like we do in our previous videos
+
+so here we do the rendering on top of the component return section , because we  know the condtions that return also store in any variable and store them in another new variable
+
