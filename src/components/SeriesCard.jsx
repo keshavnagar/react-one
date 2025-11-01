24ce1c86ce2ee6d007abfe4402ffea259e5b19d6
keshavnagar <keshavnagar887@gmail.com>
2025-11-01 12:42:25 +0530

    complete the homework

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,19 +1,19 @@
-import "./SeriesCard.css"
-const SeriesCard = ({series}) => {
+import "./SeriesCard.css";
+const SeriesCard = ({ series }) => {
   const { name, rating, description, genre, cast, watch_url, img_url } = series;
   return (
     <li className="grid-item">
+      <img width={400} height={200} src={img_url} alt={name} />
       <div>
-        <img width={400} height={200} src={img_url} alt={name} />
+        <h1>Name: {name}</h1>
+        <h2>Rating: {rating}</h2>
+        <p>Desc: {description}</p>
+        <p>Genre: {genre}</p>
+        <p>Cast: {cast}</p>
+        <a href={watch_url} target="_blank">
+          <button>watch now</button>
+        </a>
       </div>
-      <h1>Name: {name}</h1>
-      <h2>Rating: {rating}</h2>
-      <p>Desc: {description}</p>
-      <p>Genre: {genre}</p>
-      <p>Cast: {cast}</p>
-      <a href={watch_url} target="_blank">
-        <button>watch now</button>
-      </a>
     </li>
   );
 };
