af338dd14257095039ce04503480e9bb99e04eb7
keshavnagar <keshavnagar887@gmail.com>
2025-11-06 00:02:49 +0530

    previous commit is wrong about like , i want to do this thing instead of that

--- src/components/EventHandling.jsx
+++ src/components/EventHandling.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 
 const EventHandling = () => {
+  const handleClick = (event) => {
+    console.log(event);
+    console.log("button clicked");
+  };
   return (
     <>
       <button
-        onClick={() => {
-          console.log("hello onClick");
-        }}
+        onClick={handleClick}
         className="m-8 px-5 py-2.5 bg-indigo-600 text-white font-medium rounded-full shadow hover:shadow-lg hover:bg-indigo-700 transition"
       >
         Click Here
+      </button>
+      <button
+        onClick={() => handleClick}
+        className="m-8 px-5 py-2.5 bg-indigo-600 text-white font-medium rounded-full shadow hover:shadow-lg hover:bg-indigo-700 transition"
+      >
+        Click Here click here
       </button>
     </>
   );
