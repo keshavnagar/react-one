9a01bbc821ce121b08333b453bda7f5c2f5f195b
keshavnagar <keshavnagar887@gmail.com>
2025-11-06 00:05:53 +0530

    add event for right output

--- src/components/EventHandling.jsx
+++ src/components/EventHandling.jsx
@@ -14,7 +14,7 @@ const EventHandling = () => {
         Click Here
       </button>
       <button
-        onClick={() => handleClick()}
+        onClick={(event) => handleClick(event)}
         className="m-8 px-5 py-2.5 bg-indigo-600 text-white font-medium rounded-full shadow hover:shadow-lg hover:bg-indigo-700 transition"
       >
         Click Here click here
