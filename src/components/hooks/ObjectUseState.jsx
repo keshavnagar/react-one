7be7052e97b251c16a0e4625afacdb2f6ae82a4b
keshavnagar <keshavnagar887@gmail.com>
2025-11-07 19:00:26 +0530

    complete the challenge with usestate

--- src/components/hooks/ObjectUseState.jsx
+++ src/components/hooks/ObjectUseState.jsx
@@ -1,15 +1,16 @@
+import { useState } from "react";
 const ObjectUseState = () => {
-  const Users = [
+  const [users, setUsers] = useState([
     { name: "harsh", age: 12 },
     { name: "vishwas", age: 22 },
-    { name: "vijay", age: 32 },
+    { name: "raj", age: 32 },
     { name: "ajay", age: 23 },
     { name: "vikas", age: 19 },
-  ];
+  ]);
   return (
     <>
       <ul>
-        {Users.map((user) => (
+        {users.map((user) => (
           <li>
             {user.name}-{user.age}
           </li>
