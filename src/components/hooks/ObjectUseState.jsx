83852122ae1891f8128523d019a273d1dde20726
keshavnagar <keshavnagar887@gmail.com>
2025-11-10 21:33:41 +0530

    found the average of age of users

--- src/components/hooks/ObjectUseState.jsx
+++ src/components/hooks/ObjectUseState.jsx
@@ -6,17 +6,26 @@ const ObjectUseState = () => {
     { name: "raj", age: 32 },
     { name: "ajay", age: 23 },
     { name: "vikas", age: 19 },
+      { name: "karina", age: 23 },
+    { name: "akshat", age: 19 },
   ]);
+  const [aveAge, setAveAge] = useState(0);
+  const aveAgeCal = () => {
+    let total = 0;
+    users.map((user) => (total = total + user.age));
+    setAveAge(total / users.length);
+  };
   return (
     <>
-      <ul>
-        {users.map((user) => (
-          <li>
-            {323}
-            {user.name}-{user.age}
-          </li>
-        ))}
-      </ul>
+      <h1>{aveAge}</h1>
+      <button
+        className="px-5 py-2 rounded-lg bg-blue-600 text-white font-medium
+         hover:bg-blue-700 active:bg-blue-800
+         transition-colors duration-200"
+        onClick={aveAgeCal}
+      >
+        Find Average
+      </button>
     </>
   );
 };
