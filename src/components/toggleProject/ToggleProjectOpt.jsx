0c7cc4ff8ff56153fea742cdf27d0b65c757e452
keshavnagar <keshavnagar887@gmail.com>
2025-11-14 18:13:22 +0530

    remove again the knowledge of the on and off intead of true and false

--- src/components/toggleProject/ToggleProjectOpt.jsx
+++ src/components/toggleProject/ToggleProjectOpt.jsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
 
 const ToggleProjectOpt = () => {
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState("off");
   return (
     <>
       <div className="flex justify-center items-center h-screen w-screen">
         <div className={` h-50 w-50 p-2 ${toggle==="off"? `bg-red-400`: `bg-green-400`}`}
           onClickCapture={() => {
-            setToggle(toggle ? "on" : "off");
+            setToggle(toggle === "off" ? "on" : "off");
             alert("parent");
           }}
         >
-          <div className={`${toggle ? `bg-red-600` : `bg-green-600`}`}
-            // onClickCapture={() => {
-            //   setToggle(toggle === "off" ? "on" : "off");
-            //   alert("child");
-            // }}
+          <div className={`${toggle==="off" ? `bg-red-600` : `bg-green-600`}`}
+            onClickCapture={() => {
+              setToggle(toggle === "off" ? "on" : "off");
+              alert("child");
+            }}
           >
             <span>{toggle}</span>
           </div>
