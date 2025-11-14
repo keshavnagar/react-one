1ad7544acbe129cfcefdfc709019f8c7c1f6193e
keshavnagar <keshavnagar887@gmail.com>
2025-11-14 16:57:21 +0530

    add transhition also

--- src/components/toggleProject/ToggleProject.jsx
+++ src/components/toggleProject/ToggleProject.jsx
@@ -7,15 +7,15 @@ const ToggleProject = () => {
       <div className="h-screen w-screen flex justify-center items-center">
         <button
           className={`text-white flex p-2 w-50 items-center rounded-full ${
-            toggle === "off"
-              ? "bg-red-400 justify-start"
-              : "justify-end bg-green-400"
+            toggle === "off" ? "bg-red-400" : "bg-green-400"
           }`}
           onClick={() => setToggle(toggle === "off" ? "on" : "off")}
         >
           <strong
-            className={`p-4 w-25 h-25 flex justify-center items-center  text-3xl rounded-full ${
-              toggle === "off" ? "bg-red-600 text-red-200" : "bg-green-600 text-green-200"
+            className={`p-4 w-25 h-25 flex justify-center items-center  text-3xl rounded-full transition-trasform duration-600 ${
+              toggle === "off"
+                ? "bg-red-600 text-red-200 translate-0 "
+                : "bg-green-600 text-green-200 translate-x-21"
             }`}
           >
             {toggle}
