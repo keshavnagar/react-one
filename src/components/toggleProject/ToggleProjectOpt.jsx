63933d5db666015c308f27e2580afa3552804ca0
keshavnagar <keshavnagar887@gmail.com>
2025-11-14 18:46:33 +0530

    add the another way to on and off the switch

--- src/components/toggleProject/ToggleProjectOpt.jsx
+++ src/components/toggleProject/ToggleProjectOpt.jsx
@@ -1,23 +1,19 @@
 import { useState } from "react";
 
 const ToggleProjectOpt = () => {
-  const [toggle, setToggle] = useState("off");
+  const [toggle, setToggle] = useState(false);
+
   return (
     <>
       <div className="flex justify-center items-center h-screen w-screen">
-        <div className={` h-50 w-50 p-2 ${toggle==="off"? `bg-red-400`: `bg-green-400`}`}
+        <div
+          className={` h-50 w-50 p-2 ${toggle ? `bg-red-400` : `bg-green-400`}`}
           onClickCapture={() => {
-            setToggle(toggle === "off" ? "on" : "off");
-            alert("parent");
+            setToggle(!toggle);
           }}
         >
-          <div className={`${toggle==="off" ? `bg-red-600` : `bg-green-600`}`}
-            onClickCapture={() => {
-              setToggle(toggle === "off" ? "on" : "off");
-              alert("child");
-            }}
-          >
-            <span>{toggle}</span>
+          <div className={`${toggle ? `bg-red-600` : `bg-green-600`}`}>
+            <span>{toggle ? "on" : "off"}</span>
           </div>
         </div>
       </div>
